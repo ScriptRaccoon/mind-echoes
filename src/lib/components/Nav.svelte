13b4bbf0830abafc95eb387e406ca11bf3858389
Script Raccoon <scriptraccoon@gmail.com>
2026-01-28 19:14:38 +0100

    add icons to nav

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { PencilLine, User, CirclePlus, House, LogIn, UserPlus } from 'lucide-svelte'
 	import { page } from '$app/state'
 </script>
 
@@ -6,23 +7,26 @@
 	<ul>
 		{#if page.data.user}
 			<li>
-				<a href="/dashboard">Diary</a>
+				<a href="/dashboard"><PencilLine size={20} /> Diary</a>
 			</li>
 			<li>
-				<a href="/account">Account</a>
+				<a href="/account">
+					<User size={20} />
+					Account</a
+				>
 			</li>
 			<li>
-				<a href="/entry/new">New entry</a>
+				<a href="/entry/new"><CirclePlus size={20} /> New entry</a>
 			</li>
 		{:else}
 			<li>
-				<a href="/">Home</a>
+				<a href="/"><House size={20} /> Home</a>
 			</li>
 			<li>
-				<a href="/login">Login</a>
+				<a href="/login"><LogIn size={20} /> Login</a>
 			</li>
 			<li>
-				<a href="/register">Register</a>
+				<a href="/register"><UserPlus size={20} /> Register</a>
 			</li>
 		{/if}
 	</ul>
@@ -38,5 +42,16 @@
 		display: flex;
 		justify-content: center;
 		gap: 1rem;
+	}
+
+	a {
+		display: inline-flex;
+		gap: 0.4rem;
+		align-items: center;
+		text-decoration: none;
+		outline: 1px solid var(--dark-outline-color);
+		border-radius: 0.5rem;
+		padding: 0.25rem 0.6rem;
+		background-color: var(--input-bg-color);
 	}
 </style>
