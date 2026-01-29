d09bbc76cb87e493390c80fe0962040473e6c47b
Script Raccoon <scriptraccoon@gmail.com>
2026-01-29 13:20:38 +0100

    send mail again

--- src/routes/register/step-4/+page.svelte
+++ src/routes/register/step-4/+page.svelte
@@ -3,6 +3,10 @@
 	import RegistrationHeader from '$lib/components/RegistrationHeader.svelte'
 
 	let { form } = $props()
+
+	function send_mail_again() {
+		window.location.reload()
+	}
 </script>
 
 <RegistrationHeader step={4} />
@@ -10,6 +14,10 @@
 <p>
 	To verify your email address, we have sent you a registration code. Enter it below to
 	complete the registration.
+</p>
+
+<p>
+	<button class="button" onclick={send_mail_again}>Send email again</button>
 </p>
 
 <form method="POST" use:enhance>
