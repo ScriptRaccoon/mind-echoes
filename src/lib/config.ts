474370d661c62dc699eecfe689ac4dad297ab399
Script Raccoon <scriptraccoon@gmail.com>
2026-02-01 12:12:38 +0100

    add meta tags

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,1 +1,6 @@
 export const APP_TITLE = 'Mind Echoes'
+
+export const APP_DESCRIPTION =
+	'Capture your thoughts, reflect on your gratitude, and explore your mind, all in one secure space.'
+
+export const APP_DOMAIN = 'https://mind-echoes.netlify.app'
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import './app.css'
 	import Nav from '$lib/components/Nav.svelte'
 	import Dialog from '$lib/components/Dialog.svelte'
-	import { APP_TITLE } from '$lib/config'
+	import { APP_DESCRIPTION, APP_DOMAIN, APP_TITLE } from '$lib/config'
 	import LoadingBar from '$lib/components/LoadingBar.svelte'
 
 	let { children } = $props()
@@ -17,6 +17,14 @@
 	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 	<meta name="apple-mobile-web-app-title" content={APP_TITLE} />
 	<link rel="manifest" href="/site.webmanifest" />
+
+	<meta property="og:title" content={APP_TITLE} />
+	<meta property="og:description" content={APP_DESCRIPTION} />
+	<meta name="description" content={APP_DESCRIPTION} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={APP_DOMAIN} />
+	<meta property="og:site_name" content={APP_TITLE} />
+	<meta property="og:image" content="{APP_DOMAIN}/screenshot.png" />
 </svelte:head>
 
 <Nav />
