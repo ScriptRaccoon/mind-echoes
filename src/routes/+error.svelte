6b2b140c6ef947ebc4cbd86a702a4ab66b1b2e67
Script Raccoon <scriptraccoon@gmail.com>
2026-02-01 13:45:07 +0100

    add custom error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { page } from '$app/state'
+
+	function go_back() {
+		window.history.back()
+	}
+</script>
+
+<header>
+	<h1>{page.status}</h1>
+</header>
+
+<p>
+	{page.error?.message}
+</p>
+
+<p>
+	<button class="button" onclick={go_back}>Go back</button>
+</p>
